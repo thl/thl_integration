7f464aac340bb6540c7da60991ae3e9b4899112a
Tom Benner <tbb4n@virginia.edu>
2010-02-04 16:47:44 -0500

    Fixed bugs with handling GET params in Thl::UtilsController#proxy

--- app/controllers/thl/utils_controller.rb
+++ app/controllers/thl/utils_controller.rb
@@ -1,13 +1,20 @@
 class Thl::UtilsController < ApplicationController
   
   def proxy
+    
+    # We want to grab params that are part of the requested URL, but ignore ones that are supplied by Rails
     ignored_params = ["proxy_url", "action", "controller"]
     url_params = params.reject{|param, val| ignored_params.include?(param) }.collect{ |param, val| param + '=' + CGI.escape(val) }.join('&')
-    url = params[:proxy_url].join('/') + '?' + url_params
+    
+    url = params[:proxy_url]
+    url += '&' + url_params unless url_params.blank?
     url = "http://www.thlib.org" + url if url[0,1] == "/"
-    uri = URI.parse(url)
+    
+    # Parse the URL with URI.parse() so we can work with its parts more easily
+    uri = URI.parse(URI.encode(url));
     requested_host = uri.host
     headers = {}
+    
     # Check to see if the request is for a URL on thlib.org or a subdomain; if so, and if
     # this is being run on sds[3-8], make the appropriate changes to headers and uri.host
     if requested_host =~ /thlib.org/
@@ -17,13 +24,18 @@ class Thl::UtilsController < ApplicationController
         uri.host = '127.0.0.1'
       end
     end
+    
     # Required for requests without paths (e.g. http://www.google.com)
     uri.path = "/" if uri.path.empty?
-    request = Net::HTTP::Get.new(uri.path + '?' + uri.query, headers)
+    
+    path = uri.query.blank? ? uri.path : uri.path + '?' + uri.query 
+    request = Net::HTTP::Get.new(path, headers)
     result = Net::HTTP.start(uri.host, uri.port) {|http|
       http.request(request)
     }
+    
     render :text => result.body
+    
   end
   
 end
