82f16b815a935505c5dd0ad1527328151bc5028a
Andres Montano <amontano@virginia.edu>
2010-02-10 11:17:43 -0600

    Adding active resource patch to thl integration to avoid having it in every single active resource plugin.

--- init.rb
+++ init.rb
@@ -2,6 +2,7 @@
 require 'open-uri'
 require 'hpricot'
 require 'patches/traverse'
+require 'patches/active_resource_patch'
 require 'thdl_integration'
 ActionView::Base.send :include, ThdlIntegrationHelper
 ActionView::Base.send :include, FrameHelper
