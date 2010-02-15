6c49d8a385d7865c19289c745323e185e86603b1
Tom Benner <tbb4n@virginia.edu>
2010-02-15 11:20:16 -0500

    Removed route globbing from the utils/proxy route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
 
   map.namespace(:thl) do |thl|
-    thl.connect 'utils/proxy/*proxy_url', :controller => 'utils', :action => 'proxy'
+    thl.connect 'utils/proxy/', :controller => 'utils', :action => 'proxy'
   end
 
 end
