aa2b1ea1264cd1767b4883895f2723816c4dc0c7
Andres Montano <amontano@virginia.edu>
2010-06-12 22:42:18 -0600

    taking common functionality out and putting it in thl/active_resource_extensions

--- init.rb
+++ init.rb
@@ -1,8 +1,6 @@
 # Include hook code here
 require 'open-uri'
 require 'hpricot'
-require 'patches/traverse'
-require 'patches/active_resource_patch'
 require 'thdl_integration'
 ActionView::Base.send :include, ThdlIntegrationHelper
 ActionView::Base.send :include, FrameHelper
