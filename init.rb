959ed24b76ee1295c75a14db41a3e19e41f54e24
Andres Montano <amontano@virginia.edu>
2010-12-21 21:09:13 -0600

    getting rid of domain session and moved it to environment

--- init.rb
+++ init.rb
@@ -5,5 +5,3 @@ require 'thdl_integration'
 ActionView::Base.send :include, ThdlIntegrationHelper
 ActionView::Base.send :include, FrameHelper
 ActionView::Base.send :include, TinyMceExtensionHelper
-
-ActionController::Base.session = { :domain => ".thlib.org" }
