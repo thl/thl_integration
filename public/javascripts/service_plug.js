742b25a88e2092abf7a877143a53bbcffbb650c6
Jed Verity <jev3a@virginia.edu>
2011-06-06 18:12:51 -0400

    adding fix for link rewrites

--- public/javascripts/service_plug.js
+++ public/javascripts/service_plug.js
@@ -107,9 +107,12 @@ var frame_service = {
 	
 	rewrite_links: function(href) {
 		if ( parent_url.length && href.indexOf('#') != 0 && href.indexOf(parent_url) == -1 ) {
-			href += ( href.indexOf('?') > -1 ? '&' : '?' ) + "parent_url=" + parent_url;
+			var arg = "parent_url=" + parent_url,
+                pieces = href.split('#'),
+                newarg = ( href.indexOf('?') > -1 ? '&' : '?' ) + arg + ( href.indexOf('#') > -1 ? '#' : ''),
+			    newlink = ( pieces.length > 1 ? pieces.join(newarg) : pieces[0] + newarg);
 		}
-		return href
+		return newlink;
 	},
 
 	hide_stuff: function() {
