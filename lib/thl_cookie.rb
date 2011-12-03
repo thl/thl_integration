7b16d4dd61e1346197d0e0a60b1b328b9940687a
Andres Montano <amontano@virginia.edu>
2011-12-03 17:45:47 -0600

    adding new uncommitted safer cookie.

---
+++ lib/thl_cookie.rb
@@ -0,0 +1,16 @@
+module ThlCookie
+  def self.session_key
+    'thl_session'
+  end
+  
+  def self.session_secret
+    secret_file = File.join(RAILS_ROOT, "secret.txt")
+    if File.exist?(secret_file)
+      secret = File.read(secret_file)
+    else
+      secret = ActiveSupport::SecureRandom.hex(64)
+      File.open(secret_file, 'w') { |f| f.write(secret) }
+    end
+    secret
+  end  
+end
