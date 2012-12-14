62dbcd96645c45c4a9d03faff6aade4d4c84b120
Andres Montano <amontano@virginia.edu>
2012-12-14 09:23:32 -0600

    seems side menu is now toggled by default

--- app/assets/javascripts/thl_integration/application.js
+++ app/assets/javascripts/thl_integration/application.js
@@ -10,8 +10,3 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-jQuery(document).ready(function(){
-	if(typeof(ToggleSideMenu) != "undefined"){
-		ToggleSideMenu();
-	}
-});
